728d41a683a65db97c5dd5fd16d865866d506258
amit1008 <amitkmrtripathi@gmail.com>
2020-05-02 13:01:16 +0530

    combo chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ComboChartComponent, ComboSeriesVerticalComponent } from '../app/combo-chart';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxChartsModule } from '../../projects/swimlane/ngx-charts/src/lib/ngx-charts.module';
+//import { NgxUIModule } from '@swimlane/ngx-ui';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComboChartComponent,
+    ComboSeriesVerticalComponent
   ],
   imports: [
+    NgxChartsModule,
+  //  NgxUIModule,
     BrowserModule,
     AppRoutingModule
   ],
